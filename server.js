631da581eae9e998ddaadc5253df84ad531507cf
Maxim Stribylo <max057521@gmail.com>
2022-10-29 01:37:53 +0300

    update

--- server.js
+++ server.js
@@ -2,19 +2,19 @@
 import ws from "ws";
 import { v4 as uuid } from "uuid";
 const { Server } = ws;
-const clients = {};
+const clients = {}; // Объект клиентов
 
 const wss = new Server({ PORT: 8000 });
 wss.on("connection", (ws) => {
   const id = uuid();
-  clients[id] = ws;
+  clients[id] = ws; // Сокет соединился с клиентом по ид - новый клиент
 
   console.log(`New client ${id}`);
 
   ws.on("message", (rawMessage) => {});
 
   ws.on("close", () => {
-    delete clients[id];
+    delete clients[id]; // клиент закрылся
     console.log(`Client is closed ${id}`);
   });
 });

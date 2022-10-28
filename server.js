4fb28eec2ece000ca8604dd0c8266f1d07473431
Maxim Stribylo <max057521@gmail.com>
2022-10-29 00:35:58 +0300

    add files

---
+++ server.js
@@ -0,0 +1,20 @@
+// ECMAScript ES6 module
+import ws from "ws";
+import { v4 as uuid } from "uuid";
+const { Server } = ws;
+const clients = {};
+
+const wss = new Server({ PORT: 8000 });
+wss.on("connection", (ws) => {
+  const id = uuid();
+  clients[id] = ws;
+
+  console.log(`New client ${id}`);
+
+  ws.on("message", (rawMessage) => {});
+
+  ws.on("close", () => {
+    delete clients[id];
+    console.log(`Client is closed ${id}`);
+  });
+});

4b8d8b67c098de9af585e6acaee679985395f051
Maxim Stribylo <max057521@gmail.com>
2022-10-29 23:46:58 +0300

    update

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
-// ECMAScript ES6 module
-import ws from "ws";
-import { v4 as uuid } from "uuid";
-const { Server } = ws;
-const clients = {}; // Объект клиентов
+const WebSocketServer = new require("ws");
 
-const wss = new Server({ PORT: 8000 });
+const wss = new WebSocketServer.Server({ port: 8080 });
+
 wss.on("connection", (ws) => {
-  const id = uuid();
-  clients[id] = ws; // Сокет соединился с клиентом по ид - новый клиент
-
-  console.log(`New client ${id}`);
-
-  ws.on("message", (rawMessage) => {
-    console.log(rawMessage);
-  });
-
-  ws.on("close", () => {
-    delete clients[id]; // клиент закрылся
-    console.log(`Client is closed ${id}`);
-  });
+  console.log("Hoвoe соединение");
 });

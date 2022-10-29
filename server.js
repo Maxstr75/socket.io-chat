af5ff13b52d57fe2e385c0c43afc3f1dbac1e6b7
Maxim Stribylo <max057521@gmail.com>
2022-10-30 01:02:10 +0300

    update

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-// const WebSocketServer = new require("ws");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
-// const wss = new WebSocketServer.Server({ port: 8080 });
+server.listen(process.env.PORT || 3000, function () {
+  console.log("Server running in port 3000");
+});
 
-// let clients = [];
+app.use(express.static(__dirname + "/public"));
 
-// wss.on("connection", (ws) => {
-//   let id = clients.length;
-//   clients[id] = ws;
-//   console.log(`Hoвoe соединение #${id}`);
-//   // отправляем клиенту сообщение
-//   clients[id].send(`Привет, вам присвоен номер №${id}`);
-//   // отправляем все остальным
-//   clients.forEach((item, index) => {
-//     if (index !== id) {
-//       item.send(`К нам присоединился номер - ${id}`);
-//     }
-//   });
-// });
+// Подключаемые пользователи
+const users = {};
+
+// Все работа чата будет заключена внутри этой конструкции:
+// Этот код выполнится для каждого вновь подключаемого пользователя по событию connection.
+io.sockets.on("connection", (client) => {
+  // Это функция выполняет событие event и
+  // пересылает данные data конкретно для текущего пользователя client.emit(event, data),
+  //  а после инициирует событие для всех остальных подключившихся пользователей client.broadcast.emit(event, data)
+  const broadcast = (event, data) => {
+    client.emit(event, data);
+    client.broadcast.emit(event, data);
+  };
+  broadcast("user", users); //При первом подключении пользователя мы выполняем событие user и сообщаем всем участникам чата, наш текущий список пользователей
+
+  // Отправка сообщения
+  client.on("message", (message) => {
+    if (users[client.id] !== message.name) {
+      users[client.id] = message.name;
+      broadcast("user", users);
+    }
+    broadcast("message", message);
+  });
+  // Пользователь вышел из чата
+  client.on("disconnect", () => {
+    delete users[client.id];
+    client.broadcast.emit("user", users);
+  });
+});

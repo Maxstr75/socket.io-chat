527f65fedf5077533fe337029d749d58367e478b
Maxim Stribylo <max057521@gmail.com>
2022-10-30 00:00:15 +0300

    update

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const WebSocketServer = new require("ws");
 
 const wss = new WebSocketServer.Server({ port: 8080 });
 
+let clients = [];
+
 wss.on("connection", (ws) => {
-  console.log("Hoвoe соединение");
+  let id = clients.length;
+  clients[id] = ws;
+  console.log(`Hoвoe соединение #${id}`);
+  // отправляем клиенту сообщение
+  clients[id].send(`Привет, вам присвоен номер №${id}`);
+  // отправляем все остальным
+  clients.forEach((item, index) => {
+    if (index !== id) {
+      item.send(`К нам присоединился номер - ${id}`);
+    }
+  });
 });

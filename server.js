1289411ba8bc489896681c23efd16f5ba348e310
Maxim Stribylo <max057521@gmail.com>
2022-10-30 00:34:25 +0300

    update

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const WebSocketServer = new require("ws");
+// const WebSocketServer = new require("ws");
 
-const wss = new WebSocketServer.Server({ port: 8080 });
+// const wss = new WebSocketServer.Server({ port: 8080 });
 
-let clients = [];
+// let clients = [];
 
-wss.on("connection", (ws) => {
-  let id = clients.length;
-  clients[id] = ws;
-  console.log(`Hoвoe соединение #${id}`);
-  // отправляем клиенту сообщение
-  clients[id].send(`Привет, вам присвоен номер №${id}`);
-  // отправляем все остальным
-  clients.forEach((item, index) => {
-    if (index !== id) {
-      item.send(`К нам присоединился номер - ${id}`);
-    }
-  });
-});
+// wss.on("connection", (ws) => {
+//   let id = clients.length;
+//   clients[id] = ws;
+//   console.log(`Hoвoe соединение #${id}`);
+//   // отправляем клиенту сообщение
+//   clients[id].send(`Привет, вам присвоен номер №${id}`);
+//   // отправляем все остальным
+//   clients.forEach((item, index) => {
+//     if (index !== id) {
+//       item.send(`К нам присоединился номер - ${id}`);
+//     }
+//   });
+// });

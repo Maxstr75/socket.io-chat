b574b46c8d64446388f99379f9244694b6f6265b
Maxim Stribylo <max057521@gmail.com>
2022-10-30 03:45:42 +0300

    update

--- server.js
+++ server.js
@@ -2,40 +2,19 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-server.listen(process.env.PORT || 3000, function () {
-  console.log("Server running in port 3000");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.use(express.static(__dirname + "/public"));
-
-// Подключаемые пользователи
-const users = {};
-
-// Все работа чата будет заключена внутри этой конструкции:
-// Этот код выполнится для каждого вновь подключаемого пользователя по событию connection.
-io.sockets.on("connection", (client) => {
-  // Это функция выполняет событие event и
-  // пересылает данные data конкретно для текущего пользователя client.emit(event, data),
-  //  а после инициирует событие для всех остальных подключившихся пользователей client.broadcast.emit(event, data)
-  const broadcast = (event, data) => {
-    client.emit(event, data);
-    client.broadcast.emit(event, data);
-  };
-  broadcast("user", users); //При первом подключении пользователя мы выполняем событие user и сообщаем всем участникам чата, наш текущий список пользователей
-
-  // Отправка сообщения
-  client.on("message", (message) => {
-    if (users[client.id] !== message.name) {
-      users[client.id] = message.name;
-      broadcast("user", users);
-    }
-    broadcast("message", message);
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
-  // Пользователь вышел из чата
-  client.on("disconnect", () => {
-    delete users[client.id];
-    client.broadcast.emit("user", users);
-  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
